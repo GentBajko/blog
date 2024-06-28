1fc161837aae9746e114a8b8b19e09806ebe5193
Gent Bajko <gent.bajko@gmail.com>
2024-06-28 17:58:31 +0200

    fixed styling

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { Footer, Layout, MarkdownEditor } from "@/components";
-import { Navbar } from "@/components/Navbar";
+import { Layout, MarkdownEditor } from "@/components";
 import "prismjs/themes/prism-tomorrow.css";
 import { useState } from "react";
 
@@ -24,41 +23,43 @@ export default function Editor() {
   };
 
   if (!authenticated) {
-  return (
-    <Layout>
-      <div className="flex flex-col flex-1 justify-center items-center">
-        <div className="container mx-auto p-4">
-          <h1 className="text-2xl font-bold mb-4">Login</h1>
-          <input
-            type="password"
-            className="w-full p-2 border border-gray-300 rounded mb-4"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            onKeyDown={handleKeyDown}
-          />
-          <button
-            className="bg-primary-foreground text-primary py-2 px-4 rounded-full"
-            onClick={handleLogin}
-          >
-            Login
-          </button>
+    return (
+      <Layout>
+        <div className="flex flex-col h-full justify-center items-center">
+          <main className="flex-1 flex items-center justify-center">
+            <div className="container mx-auto p-4">
+              <h1 className="text-2xl font-bold mb-4">Login</h1>
+              <input
+                type="password"
+                className="w-full p-2 border border-gray-300 rounded mb-4"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                onKeyDown={handleKeyDown}
+              />
+              <button
+                className="bg-primary-foreground text-primary py-2 px-4 rounded-full"
+                onClick={handleLogin}
+              >
+                Login
+              </button>
+            </div>
+          </main>
         </div>
-      </div>
-    </Layout>
-  );
+      </Layout>
+    );
   }
 
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-1">
-        <section className="bg-background py-8 md:py-12">
-          <div className="container mx-auto px-4">
-            <MarkdownEditor />
-          </div>
-        </section>
-      </main>
-      <Footer />
-    </div>
+    <Layout>
+      <div className="flex flex-col">
+        <main className="flex-1">
+          <section className="bg-background py-8 md:py-12">
+            <div className="container mx-auto px-4">
+              <MarkdownEditor />
+            </div>
+          </section>
+        </main>
+      </div>
+    </Layout>
   );
 }
